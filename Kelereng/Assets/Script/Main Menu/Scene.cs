d412687f8f601c38cb499b1a9f45e9f47dafc03c
Muhammad Abbyu J <mabbyu27@gmail.com>
2021-04-05 18:59:01 +0700

    belum fix. jaga2 takut laptop bluescreen

---
+++ Kelereng/Assets/Script/Main Menu/Scene.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //pindah scene menu
+    public void MenuScene()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    //pindah scene galeri
+    public void GaleriScene()
+    {
+        SceneManager.LoadScene("Galeri");
+    }
+
+    //pindah scene setting
+    public void SettingScene()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+
+    //pindah scene pilih level
+    public void PilihLevelScene()
+    {
+        SceneManager.LoadScene("Pilih Level");
+    }
+
+    //pindah scene in game level 1
+    public void Level1Scene()
+    {
+        SceneManager.LoadScene("In Game Level 1");
+    }
+    
+    //pindah scene in game level 2
+    public void Level2Scene()
+    {
+        SceneManager.LoadScene("In Game Level 2");
+    }
+
+    //keluar dari game
+    public void KeluarScene()
+    {
+        Application.Quit();
+    }
+}
