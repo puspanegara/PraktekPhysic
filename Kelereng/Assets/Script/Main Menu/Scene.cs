9bc67f91bde3f698427519026c00e6414686560a
Muhammad Abbyu J <mabbyu27@gmail.com>
2021-04-05 21:24:18 +0700

    belum fix 2.0

--- Kelereng/Assets/Script/Main Menu/Scene.cs
+++ Kelereng/Assets/Script/Main Menu/Scene.cs
@@ -5,16 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class Scene : MonoBehaviour
 {
+    public GameObject pauseGame;
+
     // Start is called before the first frame update
     void Start()
     {
-      
+        pauseGame.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void PauseGame()
+    {
+        pauseGame.SetActive(true);
     }
 
     //pindah scene menu
