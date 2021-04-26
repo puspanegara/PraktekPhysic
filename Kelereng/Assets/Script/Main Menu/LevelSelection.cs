834b224fafdb3324d5abd977cf8663329c9ce699
puspanegara <54850975+puspanegara@users.noreply.github.com>
2021-04-26 10:51:20 +0700

    > Create Level Selection:  Another button level gonna disable > Added new Sprite :  > Display  > Animation >Added new sound :  > BGM Main Menu  > BGM in Pasir  > BGM in Lantai

---
+++ Kelereng/Assets/Script/Main Menu/LevelSelection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour
+{
+    public Button[] levelButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int levelAt = PlayerPrefs.GetInt("Level At", 2);
+
+        for (int i=0; i < levelButtons.Length; i++)
+        {
+            if (i + 2 > levelAt)
+                levelButtons[i].interactable = false;
+        }
+    }
+}
